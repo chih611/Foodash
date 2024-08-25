49ba3b54018b42db28590710e04f8d14003d5760
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-08-26 00:14:46 +1000

    set up the base front end with redux, scss, bootstrap and basic backend

---
+++ redux/slices/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    userInfo: null,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    clearUser: (state) => {
+      state.userInfo = null;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
