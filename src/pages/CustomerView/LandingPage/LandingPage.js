881bbf072644f8462855faf76b7adc86a253968a
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-09-05 14:27:02 +1000

    fixed the styling for the navigation bar on the landing page and also added in the logo, icon and icon library

--- src/pages/CustomerView/LandingPage/LandingPage.js
+++ src/pages/CustomerView/LandingPage/LandingPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
 import LandingNavBar from "./LandingNavBar/LandingNavBar";
 import LandingMenu from "./LandingMenu/LandingMenu";
 
