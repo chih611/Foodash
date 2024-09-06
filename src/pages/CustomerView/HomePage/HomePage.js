1c8c2602657017c08eab666fe85e633a219203cb
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-09-07 03:12:03 +1000

    added in the direction link nav bar  for the home page

--- src/pages/CustomerView/HomePage/HomePage.js
+++ src/pages/CustomerView/HomePage/HomePage.js
@@ -1,7 +1,10 @@
+import React from "react";
+import HomeDirectionLink from "./HomeDirectionLink/HomeDirectionLink";
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <HomeDirectionLink />
     </div>
   );
 };
---
+++ src/pages/CustomerView/HomePage/HomeSideBar/HomeSideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomeSideBar = () => {
+  return (
+    <div className="home-sidebar">
+      <div className="home-sidebar-container">
+        <div className="home-sidebar-item">
+          <img src="https://via.placeholder.com/150" alt="Category" />
+          <h3>Category Name</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSideBar;
