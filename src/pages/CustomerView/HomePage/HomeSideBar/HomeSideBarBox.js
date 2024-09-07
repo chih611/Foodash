8c9e67aede08c3012e63aaac0690598d6b8d0850
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-09-07 14:06:35 +1000

    added interactive feature for the side bar

--- src/pages/CustomerView/HomePage/HomeSideBar/HomeSideBarBox.js
+++ src/pages/CustomerView/HomePage/HomeSideBar/HomeSideBarBox.js
@@ -1,13 +1,27 @@
-import React from "react";
-import { Row, Col, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Collapse } from "react-bootstrap";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const HomeSideBarBox = () => {
+  const [open, setOpen] = useState({
+    eventType: false,
+    dietary: false,
+    people: false,
+    checklist: false,
+  });
+
+  const toggleDropdown = (category) => {
+    setOpen((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
+
   return (
     <div className="sidebar-box">
       <div className="sidebar-box-category">
-        <Row>
+        <Row onClick={() => toggleDropdown("eventType")}>
           <Col xs={3}>
             <RemoveCircleIcon className="sidebar-box-removeicon" />
           </Col>
@@ -15,10 +29,19 @@ const HomeSideBarBox = () => {
           <Col xs={3}>
             <ArrowDropDownIcon />
           </Col>
-        </Row>{" "}
+        </Row>
+        <Collapse in={open.eventType}>
+          <div>
+            <ul>
+              <li>Event Type 1</li>
+              <li>Event Type 2</li>
+              <li>Event Type 3</li>
+            </ul>
+          </div>
+        </Collapse>
       </div>
       <div className="sidebar-box-category">
-        <Row>
+        <Row onClick={() => toggleDropdown("dietary")}>
           <Col xs={3}>
             <RemoveCircleIcon className="sidebar-box-removeicon" />
           </Col>
@@ -26,10 +49,19 @@ const HomeSideBarBox = () => {
           <Col xs={3}>
             <ArrowDropDownIcon />
           </Col>
-        </Row>{" "}
+        </Row>
+        <Collapse in={open.dietary}>
+          <div>
+            <ul>
+              <li>Dietary 1</li>
+              <li>Dietary 2</li>
+              <li>Dietary 3</li>
+            </ul>
+          </div>
+        </Collapse>
       </div>
       <div className="sidebar-box-category">
-        <Row>
+        <Row onClick={() => toggleDropdown("people")}>
           <Col xs={3}>
             <RemoveCircleIcon className="sidebar-box-removeicon" />
           </Col>
@@ -37,10 +69,19 @@ const HomeSideBarBox = () => {
           <Col xs={3}>
             <ArrowDropDownIcon />
           </Col>
-        </Row>{" "}
+        </Row>
+        <Collapse in={open.people}>
+          <div>
+            <ul>
+              <li>People 1</li>
+              <li>People 2</li>
+              <li>People 3</li>
+            </ul>
+          </div>
+        </Collapse>
       </div>
       <div className="sidebar-box-category">
-        <Row>
+        <Row onClick={() => toggleDropdown("checklist")}>
           <Col xs={3}>
             <RemoveCircleIcon className="sidebar-box-removeicon" />
           </Col>
@@ -48,7 +89,16 @@ const HomeSideBarBox = () => {
           <Col xs={3}>
             <ArrowDropDownIcon />
           </Col>
-        </Row>{" "}
+        </Row>
+        <Collapse in={open.checklist}>
+          <div>
+            <ul>
+              <li>Checklist 1</li>
+              <li>Checklist 2</li>
+              <li>Checklist 3</li>
+            </ul>
+          </div>
+        </Collapse>
       </div>
     </div>
   );
