4721c6245401f56390ddbe191bed135a5754bf4e
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-09-16 16:45:10 +1000

    adding in the nav bar for the home page

--- src/pages/CustomerView/HomePage/HomePage.js
+++ src/pages/CustomerView/HomePage/HomePage.js
@@ -1,16 +1,13 @@
-
 import React from "react";
 import HomeDirectionLink from "./HomeDirectionLink/HomeDirectionLink";
 import HomeContent from "./HomeContent/HomeContent";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import HomePageNavBar from "./HomePageNavBar";
-import HomeCategory from "./HomeCategory";
 
 const HomePage = () => {
   return (
     <div>
       <HomePageNavBar />
-      <HomeCategory />
       <h1>Home Page</h1>
       <HomeDirectionLink />
       <HomeContent />
