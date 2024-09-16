6af6fc69973adcb20230f96c09c6bb6ae965e6b0
Shu N. <shu.nguyen@proton.me>
2024-09-16 22:13:23 +1000

    outline ordertracking

---
+++ src/pages/CustomerView/OrderTracking/OrderList.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+const OrderList = () => {
+    return (
+      <div>
+        <h1>List of Order</h1>
+      </div>
+    );
+  };
+  
+export default OrderList;
