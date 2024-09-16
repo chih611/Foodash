5dc4f89fa6b8346559444f1b8f02615e16200861
Shu N. <shu.nguyen@proton.me>
2024-09-16 18:00:23 +1000

    update customer detail on desktop view

--- src/pages/CustomerView/CustomerProfile/CustomerDetail.js
+++ src/pages/CustomerView/CustomerProfile/CustomerDetail.js
@@ -28,22 +28,19 @@ const CustomerDetail = () => {
           <HomePageNavBar />
         </div>
 
-        <Row>
-          <Col xs={12} md={4} className="d-flex justify-content-center">
+        <Row className="d-flex justify-content-center d-lg-none">
+          <Col xs={12} md={8} className="d-flex justify-content-center d-lg-none">
             <div style={{ width: "100%", maxWidth: "342px", height: "342px", backgroundColor: "#e0e0e0", marginTop: "24px" }}>
               {/* Image Placeholder */}
             </div>
-          </Col>     
-        </Row>
-        <Row className="d-flex justify-content-end">
-          <Col className="d-flex flex-row-reverse bd-highlight" > 
+          </Col>    
+          <Col xs={12} md={4} className="d-flex flex-row-reverse bd-highlight" > 
             <EditRounded sx={{ color: "#025373" }}/>
-          </Col>
-          
+          </Col> 
         </Row>
 
         {/* Form Section */}
-        <Row>
+        <Row className="d-flex justify-content-center d-lg-none">
           <div style={{ width: "100%", marginTop: "48px" }}>
             <div className="d-flex justify-content-center flex-wrap">
               <CustomInput
@@ -102,6 +99,92 @@ const CustomerDetail = () => {
             </Link>
           </div>
         </Row>
+
+
+
+        {/* Desktop View */}
+        <Row className="w-100 align-items-center d-none d-lg-flex">
+          {/* Avatar and Edit Icon Section */}
+          <Col xs={12} md={5} className="d-flex justify-content-center">
+            <Col style={{ width: "100%", maxWidth: "342px", height: "342px", backgroundColor: "#e0e0e0", marginTop: "24px" }}>
+              {/* Image Placeholder */}
+            </Col>
+            <Col><EditRounded sx={{ color: "#025373" }}/></Col>
+          </Col>    
+
+          {/* Form Section */}
+          <Col xs={12} md={7}>
+            {/* First Name and Last Name */}
+            <Row className="d-flex justify-content-center">
+              <Col md={6} style={{ marginTop: "48px" }}>
+                <CustomInput
+                  label="First Name"
+                  value={f_name}
+                  onChange={(e) => setFName(e.target.value)}
+                />
+              </Col>
+              <Col md={6} style={{ marginTop: "48px" }}>
+                <CustomInput
+                  label="Last Name"
+                  value={l_name}
+                  onChange={(e) => setLName(e.target.value)}
+                />
+              </Col>
+            </Row>
+
+            {/* Company and ABN */}
+            <Row className="d-flex justify-content-center" style={{ marginTop: "24px" }}>
+              <Col md={6}>
+                <CustomInput label="Company Name" />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="ABN" />
+              </Col>
+            </Row>
+
+            {/* Email and Phone */}
+            <Row className="d-flex justify-content-center" style={{ marginTop: "24px" }}>
+              <Col md={6}>
+                <CustomInput label="Email" placeholder="Enter your email" type="email" />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="Phone" placeholder="+61" type="number" />
+              </Col>
+            </Row>
+
+            {/* Billing Address */}
+            <Row className="d-flex justify-content-center" style={{ marginTop: "24px" }}>
+              <Col xs={12}>
+                <p className="subtitle mb-0">Billing Address</p>
+                <hr />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="Address 1" type="text" />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="City/Suburb" type="text" />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="State" type="text" />
+              </Col>
+              <Col md={6}>
+                <CustomInput label="Postcode" type="text" />
+              </Col>
+            </Row>
+
+            {/* Save Button */}
+            <Row className="w-100 d-flex justify-content-center" style={{ marginTop: "24px" }}>
+              <Col xs={12}>
+                <Link href="/CustomerView/HomePage/HomePage" legacyBehavior passHref>
+                  <a className="w-100">
+                    <Button variant="primary" className="w-100">Save your profile</Button>
+                  </a>
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
 
        
       </Container>
