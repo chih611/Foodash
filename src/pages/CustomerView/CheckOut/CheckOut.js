237a8a0a85b68f09a0b165c38f83c0a2d1b3c71a
Shu N. <shu.nguyen@proton.me>
2024-09-19 21:19:03 +1000

    order information updates_incl_contacts, ordersummary_promocode

--- src/pages/CustomerView/CheckOut/CheckOut.js
+++ src/pages/CustomerView/CheckOut/CheckOut.js
@@ -10,11 +10,16 @@ import {
   Offcanvas,
 } from "react-bootstrap";
 import NavBarCheckOut from "./NavBarCheckOut";
+import HomeDirectionLink from "../HomePage/HomeDirectionLink/HomeDirectionLink";
+import CheckOutDetail from "./CheckOutDetail";
 
 const Checkout = () => {
   return (
     <div>
       <NavBarCheckOut/>
+      <HomeDirectionLink />
+      <CheckOutDetail />
+      
     </div>
   );
 };
