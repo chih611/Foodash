4b1e640a3f12179887c458a952018edcb4ae4be0
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-10-20 18:14:45 +1100

    moved the customer slice to cookie

--- store/middleware/loggerMiddleware.js
+++ store/middleware/loggerMiddleware.js
@@ -1,8 +1,21 @@
 const loggerMiddleware = (storeAPI) => (next) => (action) => {
-  // console.log("Dispatching action:", action);
-  let result = next(action);
-  // console.log("Next state:", storeAPI.getState());
-  return result;
+  if (typeof action === "function") {
+    // It's an async thunk action; don't log directly
+    const result = next(action);
+    if (result instanceof Promise) {
+      return result.then((resolvedValue) => {
+        console.log("Async Action Result:", resolvedValue);
+        return resolvedValue;
+      });
+    }
+    return result;
+  } else {
+    // It's a regular action; log it
+    console.log("Dispatching action:", action);
+    let result = next(action);
+    console.log("Next state:", storeAPI.getState());
+    return result;
+  }
 };
 
 export default loggerMiddleware;
