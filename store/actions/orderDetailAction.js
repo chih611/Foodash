7a6acb5a8ddc992bb7020c3a697cb4afee66373d
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-10-30 12:01:15 +1100

    fixed the rouing issue

--- store/actions/orderDetailAction.js
+++ store/actions/orderDetailAction.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { fetchOrderByCustomerIdAPI } from "../api/order.api";
 
 const BACKEND_PORT = process.env.NEXT_PUBLIC_REACT_APP_BACKEND_PORT;
-const BASE_URL = `http://localhost:8080`;
+const BASE_URL = `https://ec2.foodash.org`;
 
 const createOrderDetail = createAsyncThunk(
   "orderDetail/createOrderDetail",
--- store/slices/cartSlice.js
+++ store/slices/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const BACKEND_PORT = process.env.NEXT_PUBLIC_REACT_APP_BACKEND_PORT;
-const BASE_URL = `http://localhost:8080`;
+const BASE_URL = `https://ec2.foodash.org`;
 
 /* Helper Functions */
 
